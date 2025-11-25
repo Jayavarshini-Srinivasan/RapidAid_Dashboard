e6aaa07bbc9910b5a61ebb5fdf946989e08e0359
TEJESH.M <tejeshsms@gmail.com>
2025-11-25 13:40:24 +0530

    i have delete whole project

--- BackEnd/src/services/route.service.js
+++
@@ -1,9 +0,0 @@
-const { addRouteHistory } = require('../repositories/route.repository'); // /// ADDED
-
-const storeRouteHistory = async ({ patientId, hospitalId, path }) => { // /// ADDED
-  return addRouteHistory({ patientId, hospitalId, routeCoordinates: path }); // /// ADDED
-}; // /// ADDED
-
-module.exports = { // /// ADDED
-  storeRouteHistory, // /// ADDED
-}; // /// ADDED
